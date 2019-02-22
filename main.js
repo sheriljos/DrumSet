654d7b05507c175010fc43f52141ba049e4fedc9
Sheril Elizabeth Jose <sheril@Sherils-MacBook-Pro.local>
2019-02-22 19:52:05 +0100

    adding appropriate comments and grouping code

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ function removeTransition(e) {
 }
 
 function playSound(e){
+    //playing the audio corresponding to the key
     const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
-    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
-
     if (!audio) {
         return;
     }
-
     audio.currentTime = 0;
     audio.play();
+
+    //changing the style of the div corresponding to the key
+    const key = document.querySelector(`.key[data-key='${e.keyCode}']`);
     key.classList.add('playing');
 
-    const keys = document.querySelectorAll('.key')
+    //removing the style after the transition
+    const keys = document.querySelectorAll('.key')    
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 };
 
